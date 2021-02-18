bcc4f074dd89a5807655d2c34706cab1bce6b4e5
moisestech <m@moises.tech>
2021-02-18 05:47:32 -0500

    init uitls fingerJoints, style, drawHand

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+// https://github.com/nicknochnack/GestureRecognition/blob/main/src/utilities.js
+
+// Points for Fingers
+const fingerJoints = {};
+
+// Infinity Gaunlet Style
+const style = {};
+
+// Drawing Function
+export const drawHand = () => {
+  return null;
+}
