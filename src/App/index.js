7bcf24460a7b980cf947d7986d34410b4e97cb5e
moisestech <m@moises.tech>
2021-02-18 05:25:55 -0500

    React, Webcam, tf handpose model, drawhand func, exp default func comp App

---
+++ src/App/index.js
@@ -0,0 +1,29 @@
+// 1. Install fingerpose npm install fingerpose
+// 2. Add Use State
+// 3. Import emojis and finger pose import * as fp from "fingerpose";
+// 4. Setup hook and emoji object
+// 5. Update detect function for gesture handling
+// 6. Add emoji display to the screen
+
+import React, { useRef } from "react";
+
+// access to webcam
+import Webcam from "react-webcam";
+
+// running handpose detection
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+
+// draws handpose in canvas
+import { drawHand } from "./utilities";
+
+export default function App() {
+  let project_name = "Tensorflow.js React Hand Recognition";
+
+  return (  
+    {project_name}
+  )
+}
+
+// video: https://www.youtube.com/watch?v=9MTiQMxTXPE
+// github: https://github.com/nicknochnack/GestureRecognition/tree/main/src
