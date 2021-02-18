7b4303263496393d9e1e71ee29b1eace33384d81
moisestech <m@moises.tech>
2021-02-18 05:24:21 -0500

    module rules babel-loader presets react

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,42 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename:'bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: 'development',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  devServer: {
+    contentBase:'./dist',
+    port: 3030,
+    hot: true,
+    historyApiFallback: true
+  },
   module: {
     rules: [
+      {
+        test:/\.js$/,
+        exclude:/node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets:[
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              ['@babel/plugin-proposal-pipeline-operator', {proposal:'minimal'}],
+              "@babel/plugin-syntax-dynamic-import"
+            ]
+          }
+        }
+      },
       {
         test:/\.css$/,
         use: ['style-loader', 'css-loader']
@@ -17,7 +45,7 @@ module.exports = {
         test:/\.mp4/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]',
+          name:'[name].[ext]',
           outputPath:'video/'
         }
       },
@@ -26,7 +54,7 @@ module.exports = {
         loader:'file-loader',
         options: {
           name:'[name].[ext]',
-          outputPath:'image/'
+          outPath:'image/'
         }
       },
       {
@@ -34,5 +62,17 @@ module.exports = {
         loader:'file-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: "./public/index.html",
+      favicon: './public/favicon.ico'
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./public/_redirects" }],
+    })
+  ]
 }
